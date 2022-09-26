e345a51307dde338dd917b86f03dcbf4f9a235bb
human <human@DESKTOP-VGBLM31>
2022-09-26 14:10:07 +0900

    commit Message

--- src/main/java/TestClass2.java
+++ src/main/java/TestClass2.java
@@ -2,7 +2,7 @@
 public class TestClass2 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello! This is Github Practicing!");
+		System.out.println("Hello! This is Github Practicing!!!!");
 
 	}
 
