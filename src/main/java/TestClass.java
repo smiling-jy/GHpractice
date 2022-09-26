f0ae7d2bd2a46bf8ad162832e6b96d13f8a79bdc
human <human@DESKTOP-VGBLM31>
2022-09-26 14:03:36 +0900

    This is Git Message!!!

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -2,7 +2,7 @@
 public class TestClass {
 
 	public static void main(String[] args) {
-		System.out.println("Hello! This is Github Practicing!");
+		System.out.println("Hello! This is Github Practicing!!!");
 
 	}
 
