dd6bf04af4d03976855b861c8215ab00edaa90cd
human <human@DESKTOP-VGBLM31>
2022-09-26 15:32:37 +0900

    update

--- src/main/java/TestClass2.java
+++ src/main/java/TestClass2.java
@@ -4,6 +4,8 @@ public class TestClass2 {
 	public static void main(String[] args) {
 		System.out.println("Hello! This is Github Practicing!!!!");
 		System.out.println("새로 추가한거");
+		System.out.println("또용용");
+		
 
 	}
 
